bf1bda29d5f27d80d28efba699db880646ccd4e9
root <root@DESKTOP-68UL55P.localdomain>
2022-01-19 09:48:38 -0600

    implemented tests for timestamp-create timestamp-remove timestamp-get

--- testing/backend-tests/src/components/exposed.test.js
+++ testing/backend-tests/src/components/exposed.test.js
@@ -8,15 +8,24 @@ async function makePostRequest(data, endPoint){
       body: JSON.stringify(data),
       method: "POST"
     };
-    const response = await fetch("http://173.21.223.105:9000/" + endPoint,otherParam)
+    const response = await fetch("http://173.28.93.22:9000/" + endPoint,otherParam)
     const content = await response.json();
     console.log(content);
     return content;
 }
 
-test('Check following endpoints.', async () => {
-    expect((await makePostRequest({user: "matt", pass: "password", email:"matt@email.com"}, 'signup/')).valid).toEqual("YES");
-    expect((await makePostRequest({user: "matt", pass: "password"}, 'login/')).valid).toEqual("YES");
-    expect((await makePostRequest({user: "matt", pass: "password1"}, 'login/')).valid).toEqual("NO");
+// test('Check following endpoints.', async () => {
+//     expect((await makePostRequest({user: "matt", pass: "password", email:"matt@email.com"}, 'signup/')).valid).toEqual("YES");
+//     expect((await makePostRequest({user: "matt", pass: "password"}, 'login/')).valid).toEqual("YES");
+//     expect((await makePostRequest({user: "matt", pass: "password1"}, 'login/')).valid).toEqual("NO");
+// });
 
+test('Check cache endpoints.', async () => {
+  expect((await makePostRequest({user: "matt", pass:"password", url:"bnMMYJKZvnU", adTimes: [5,10,11,20]}, 'timestamp-create/')).valid).toEqual("YES");
+  expect((await makePostRequest({user: "matt", pass:"password", url:"JsUI40uSOTU", adTimes: [232,242]}, 'timestamp-create/')).valid).toEqual("YES");
+  expect((await makePostRequest({}, 'print/')).valid).toEqual("YES");
+  expect((await makePostRequest({user: "matt", pass:"password", url:"JsUI40uSOTU"}, 'timestamp-remove/')).valid).toEqual("YES");
+  //expect((await makePostRequest({}, 'print/')).valid).toEqual("YES");
+  expect((await makePostRequest({user: "matt", pass:"password", url:"bnMMYJKZvnU"}, 'timestamp-get/')).adTimes).toEqual([5,10,11,20]);
+  expect((await makePostRequest({}, 'print/')).valid).toEqual("YES");
 });
