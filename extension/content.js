26a2223ea4555d0efb254fbc023704b83a418ef9
root <root@DESKTOP-68UL55P.localdomain>
2022-01-25 07:55:49 -0600

    video bar edits

--- extension/content.js
+++ extension/content.js
@@ -20,12 +20,21 @@ async function makePostRequest(data, endPoint){
     return content;
 }
 
+var createAdFlag = false;
+
+chrome.runtime.onMessage.addListener(function(request,sender,sendResponse){
+    if(request.todo == "create-ad-flip"){
+        console.log("create ad flip recieved");
+        createAdFlag = !createAdFlag;
+    }
+});
+
 (async function run(){
     //console.log("run test");
     var video = document.getElementsByClassName("video-stream")[0];
     // console.log(video);
     //console.log(video.currentTime);
-    // var response = await makePostRequest({user: "matt", pass:"password", url:"bnMMYJKZvnU", adTimes: [109,213]}, 'timestamp-create/');
+    var response = await makePostRequest({user: "matt", pass:"password", url:"bnMMYJKZvnU", adTimes: [109.5,213.5]}, 'timestamp-create/');
     //console.log(response);
     // await makePostRequest({}, 'print/');
     // adTimes will be formatted differently later.
@@ -33,18 +42,64 @@ async function makePostRequest(data, endPoint){
     var progressList = document.getElementsByClassName("ytp-progress-list")[0];
     var adStampDiv = document.createElement("div");
     adStampDiv.className = "ytp-play-progress ytp-swatch-background-color";
-    adStampDiv.style = "background-color: blue; left: " + 100*(times[0]/video.duration) + "%; transform: scaleX(" + (times[1]-times[0])/video.duration + ");";
+    adStampDiv.style = "background-color: Aqua; left: " + 100*(times[0]/video.duration) + "%; transform: scaleX(" + (times[1]-times[0])/video.duration + ");";
     progressList.appendChild(adStampDiv);
+    document.getElementsByClassName("ytp-progress-bar-container")[0].addEventListener('mousedown', function(){
+        console.log('clicked down');
+    })
+    var xCoordFirst = 0;
+    var yCoordFirst = 0;
+    var xCoordSecond = 0;
+    var yCoordSecond = 0;
+    var timeStampFlag = false;
+    document.getElementsByClassName("ytp-progress-bar-container")[0].addEventListener('mouseup', function(event){
+        if(createAdFlag == true){
+            console.log('clicked up');
+            if(timeStampFlag == false){
+                var rect = document.getElementsByClassName("ytp-progress-bar-container")[0].getBoundingClientRect();
+                xCoordFirst = event.pageX - 12;
+                yCoordFirst = event.pageY;
+                var adStampScrubberCont = document.createElement("div");
+                adStampScrubberCont.className = "ytp-scrubber-container";
+                adStampScrubberCont.style="transform: translateX(" + xCoordFirst + "px);";  
+                var adStampScrubberButton = document.createElement("div");
+                adStampScrubberButton.className = "ytp-scrubber-button ytp-swatch-background-color";
+                adStampScrubberButton.style = "height: 13px; background-color: Aqua;";
+                adStampScrubberCont.appendChild(adStampScrubberButton);
+                document.getElementsByClassName("ytp-progress-bar-container")[0].appendChild(adStampScrubberCont);
+                timeStampFlag = true;
+            }
+            else{
+                var rect = document.getElementsByClassName("ytp-progress-bar-container")[0].getBoundingClientRect();
+                xCoordSecond = event.pageX - 12;
+                yCoordSecond = event.pageY;
+                var adStampScrubberCont = document.createElement("div");
+                adStampScrubberCont.className = "ytp-scrubber-container";
+                adStampScrubberCont.style="transform: translateX(" + xCoordSecond + "px);";  
+                var adStampScrubberButton = document.createElement("div");
+                adStampScrubberButton.className = "ytp-scrubber-button ytp-swatch-background-color";
+                adStampScrubberButton.style = "height: 13px; background-color: Aqua;";
+                adStampScrubberCont.appendChild(adStampScrubberButton);
+                document.getElementsByClassName("ytp-progress-bar-container")[0].appendChild(adStampScrubberCont);
+                adStampDiv = document.createElement("div");
+                adStampDiv.className = "ytp-play-progress ytp-swatch-background-color";
+                adStampDiv.style = "background-color: Aqua; left: " + (xCoordFirst <= xCoordSecond ? xCoordFirst : xCoordSecond) + "px; transform: scaleX(" + (Math.abs(xCoordSecond - xCoordFirst)/(rect.right - rect.left)) + ");";
+                progressList.appendChild(adStampDiv);
+                timeStampFlag = false;
+            }
+        }
+    })
+    
     while(true){
-        console.log("current time: " + video.currentTime);
-        console.log(times[0]);
-        console.log(times[1]);
-        console.log(video.currentTime >= times[0]);
-        console.log(video.currentTime < times[1]);
+        // console.log("current time: " + video.currentTime);
+        // console.log(times[0]);
+        // console.log(times[1]);
+        // console.log(video.currentTime >= times[0]);
+        // console.log(video.currentTime < times[1]);
         if(video.currentTime >= times[0] && video.currentTime < times[1]){
             video.currentTime = times[1];
         }
-        await sleep(500);
+        await sleep(125);
     }
 })();
 
--- extension/popup.js
+++ extension/popup.js
@@ -1,6 +1,28 @@
 console.log("test popup");
 
+// need to make this flag get stored in the chrome cash :(.
+var createAdFlag = false;
+
+document.getElementById("create-ad").addEventListener('click',function(){
+    chrome.tabs.query({active:true ,currentWindow:true},function(tabs){
+        chrome.tabs.sendMessage(tabs[0].id,{todo:"create-ad-flip"});
+    });
+    document.getElementById("finish-ad").style = "visibility: visible;";
+    createAdFlag = true;
+});
+
+document.getElementById("finish-ad").addEventListener('click', function(){
+    chrome.tabs.query({active:true ,currentWindow:true},function(tabs){
+        chrome.tabs.sendMessage(tabs[0].id,{todo:"create-ad-flip"});
+    });
+    this.style = "visibility: hidden;";
+    createAdFlag = false;
+});
+
 (function(){
+    if(createAdFlag){
+        document.getElementById("finish-ad").style = "visibility: visible;";
+    }
     chrome.tabs.query({active:true ,currentWindow:true},function(tabs){
         chrome.tabs.sendMessage(tabs[0].id,{todo:"test"});
     });
